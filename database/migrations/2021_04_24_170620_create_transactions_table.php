3969021a8e3d3b7c895f9fda539baf7231e7dbb8
Jorge Flores <jorge.flores@ppm.com.ec>
2021-04-24 16:10:21 -0500

    first commit

---
+++ database/migrations/2021_04_24_170620_create_transactions_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTransactionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('transactions', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained('users');
+            $table->string('transaction_type');
+            $table->string('currency_from');
+            $table->string('currency_to');
+            $table->double('ammount_from', 8, 6);
+            $table->double('ammount_to', 8, 6);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('transactions');
+    }
+}
